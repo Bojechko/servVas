a3df3adc71f88f6b698d9738445e35cec39fa94b
bojejko <yapsyh@gmail.com>
2021-04-05 22:20:35 +0300

    new tests

--- WebProject1/deezer_api.py
+++ WebProject1/deezer_api.py
@@ -5,11 +5,15 @@ import json
 client = deezer.Client()
 
 def  get_band_albums(band):
-   albums=client.search(band, relation='artist')[0].get_albums() #ищем альбомы
+   try :
+        albums=client.search(band, relation='artist')[0].get_albums() #ищем альбомы
    
-   dict_of_albums = { i :{ albums[i].title  : {albums[i].id : albums[i].cover}} for i in range(0, len(albums))}#берем у каждого найденного альбома id, название
+        dict_of_albums = { i :{ albums[i].title  : {albums[i].id : albums[i].cover}} for i in range(0, len(albums))}#берем у каждого найденного альбома id, название
+   
+        return json.dumps(dict_of_albums)
+   except Exception : 
+        return "No such band, error in get_band_albums"
 
-   return json.dumps(dict_of_albums)
 
 def  get_album(band, album):
    albums=client.search(band, relation='artist')[0].get_albums() #ищем альбомы
@@ -26,9 +30,11 @@ def  get_album(band, album):
    
 
 def  get_albums_tracks(band, album):
-   tracks=client.search(band, relation='artist')[0].get_albums()[get_album(band, album)].get_tracks() #ищем треки
-   #print(client.search(band, relation='artist')[0].get_albums()[album].get_tracks())
-   dict_of_tracks = {  tracks[i].title : { tracks[i].id : tracks[i].link} for i in range(0, len(tracks))}#берем у каждого найденного трека id, название
-   return json.dumps(dict_of_tracks)
+    try:
+        tracks=client.search(band, relation='artist')[0].get_albums()[get_album(band, album)].get_tracks() #ищем треки
+        dict_of_tracks = {  tracks[i].title : { tracks[i].id : tracks[i].link} for i in range(0, len(tracks))}#берем у каждого найденного трека id, название
+        return json.dumps(dict_of_tracks)
+    except Exception : 
+        return "No such album, error in get_albums_tracks"
 
     
--- WebProject1/serv_test.py
+++ WebProject1/serv_test.py
@@ -6,33 +6,33 @@ import pytest
 
 # init tests
 def test_get_albums():
-    assert init.get_albums('Korn') != 'No such band'
+    assert init.get_albums('Korn') != 'No such band, error in get_band_albums'
 
 def test_get_albums2():
-    assert init.get_albums('ssadsadd') == 'No such band'
+    assert init.get_albums('ssadsadd') == 'No such band, error in get_band_albums'
 
 def test_get_albums3():
-    assert init.get_albums(' ') == 'No such band'
+    assert init.get_albums(' ') == 'No such band, error in get_band_albums'
 
 def test_get_albums4():
-    assert init.get_albums('') == 'No such band'
+    assert init.get_albums('') == 'No such band, error in get_band_albums'
 
 
 
 def test_get_tracks():
-    assert init.get_tracks('Korn', 'The Nothing') != 'No such album'
+    assert init.get_tracks('Korn', 'The Nothing') != 'No such album, error in get_albums_tracks'
 
 def test_get_tracks2():
-    assert init.get_tracks('Korn', 'ф фыв') == 'No such album'
+    assert init.get_tracks('Korn', 'ф фыв') == 'No such album, error in get_albums_tracks'
 
 def test_get_tracks3():
-    assert init.get_tracks('фывфывфы', 'The Nothing') == 'No such album'
+    assert init.get_tracks('фывфывфы', 'The Nothing') == 'No such album, error in get_albums_tracks'
 
 def test_get_tracks4():
-    assert init.get_tracks('', '') == 'No such album'
+    assert init.get_tracks('', '') == 'No such album, error in get_albums_tracks'
 
 def test_get_tracks5():
-    assert init.get_tracks('Korn', '') == 'No such album'
+    assert init.get_tracks('Korn', '') == 'No such album, error in get_albums_tracks'
 
 
 #deezer_api tests
@@ -42,6 +42,53 @@ def test_get_album():
 
 def test_get_album2():
     assert deezer_api.get_album('Korn', 'asdasd') == "No album found"
+
+
+
+def test_get_band_albums():
+    assert deezer_api.get_band_albums('Korn') != "No such band, error in get_band_albums"
+    
+def test_get_band_albums2():
+    assert deezer_api.get_band_albums(' ') == "No such band, error in get_band_albums"
+    
+def test_get_band_albums3():
+    assert deezer_api.get_band_albums('')  == "No such band, error in get_band_albums"
+
+def test_get_band_albums4():
+    assert deezer_api.get_band_albums('точно не существет')  == "No such band, error in get_band_albums"
+
+
+def test_get_albums_tracks():
+    assert deezer_api.get_albums_tracks('Korn', 'The Nothing') != "No such album, error in get_albums_tracks"
+    
+def test_get_albums_tracks2():
+    assert deezer_api.get_albums_tracks(' ', ' ') == "No such album, error in get_albums_tracks"
+    
+def test_get_albums_tracks3():
+    assert deezer_api.get_albums_tracks('', '')  == "No such album, error in get_albums_tracks"
+
+def test_get_albums_tracks4():
+    assert deezer_api.get_albums_tracks('точно не существет', 'этого тоже нет')  == "No such album, error in get_albums_tracks"
+
+def test_get_albums_tracks6():
+    assert deezer_api.get_albums_tracks('Korn', '') == "No such album, error in get_albums_tracks"
+
+def test_get_albums_tracks7():
+    assert deezer_api.get_albums_tracks('Korn', ' ') == "No such album, error in get_albums_tracks"
+
+def test_get_albums_tracks8():
+    assert deezer_api.get_albums_tracks('', 'The Nothing') == "No such album, error in get_albums_tracks"
+
+def test_get_albums_tracks9():
+    assert deezer_api.get_albums_tracks('Korn', 'точно не существет') == "No such album, error in get_albums_tracks"
+
+def test_get_albums_tracks9():
+    assert deezer_api.get_albums_tracks('точно не существет', 'The Nothing') == "No such album, error in get_albums_tracks"
+
+
+
+
+
 
 
 
